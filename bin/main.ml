3d8455409889f5de3fe40866eaa8cebb707a76b4
Téo Pisenti <pisentt@bde.enseeiht.fr>
2024-06-21 23:01:24 +0700

    tentative échouée d'ajouter le parseur json

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,4 @@
 open Lwt
-open Cohttp
-open Cohttp_lwt_unix
 
 let graphql = {|
   {
@@ -26,12 +24,12 @@ let req_body = Printf.sprintf {|
 |} (String.escaped graphql)
 
 let body =
-  Client.post ~headers:(Cohttp.Header.init_with "Content-Type" "application/json")
+  Cohttp_lwt_unix.Client.post ~headers:(Cohttp.Header.init_with "Content-Type" "application/json")
   ~body:(Cohttp_lwt.Body.of_string req_body) (Uri.of_string "https://churros.inpt.fr/graphql")
   >>= fun (resp, body) ->
-  let code = resp |> Response.status |> Code.code_of_status in
+  let code = resp |> Cohttp_lwt_unix.Response.status |> Cohttp.Code.code_of_status in
   Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  Printf.printf "Headers: %s\n" (resp |> Cohttp_lwt_unix.Response.headers |> Cohttp.Header.to_string);
   body |> Cohttp_lwt.Body.to_string >|= fun body ->
   Printf.printf "Body of length: %d\n" (String.length body);
   body
@@ -39,3 +37,9 @@ let body =
 let () =
   let body = Lwt_main.run body in
   print_endline ("Received body\n" ^ body)
+
+(*
+let () =
+  let parsed = Option.get (Lexer.from_string Parser.file "") in
+  print_endline (Syntax.show_t_json parsed)
+*)
