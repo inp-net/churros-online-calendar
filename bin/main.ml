3560bd5314c04d77d0a5416dbaea3c7904715c5a
Téo Pisenti <pisentt@bde.enseeiht.fr>
2024-06-22 11:54:46 +0700

    premier test pour renvoyer un ics quand on se connecte en http

--- bin/main.ml
+++ bin/main.ml
@@ -33,9 +33,36 @@ let body =
   ~body:(Cohttp_lwt.Body.of_string req_body) (Uri.of_string "https://churros.inpt.fr/graphql")
   >>= fun (_, body) -> body |> Cohttp_lwt.Body.to_string
 
+
+(*
 let () =
   let body = Lwt_main.run body in
   let parsed = Option.get (Lexer.from_string Parser.file body) in
   let file = open_out "churros_calendar.ics" in
   Printf.fprintf file "%s" (Ics.print_ics (Calendar.ics_of_json parsed));
   close_out file
+*)
+
+
+let get_ics_string () =
+  Lwt_main.run body |> fun s -> Option.get (Lexer.from_string Parser.file s)
+  |> Calendar.ics_of_json |> Ics.print_ics
+
+let ics_string = get_ics_string ()
+
+let server =
+  let callback _ req _ = match Cohttp_lwt_unix.Request.meth req with
+  | `GET -> (* get_ics_string () |> fun body -> Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body () *)
+    (*
+    let uri = req |> Cohttp_lwt_unix.Request.uri |> Uri.to_string in
+    let meth = req |> Cohttp_lwt_unix.Request.meth |> Cohttp.Code.string_of_method in
+    let body = Printf.sprintf "Uri: %s\nMethod: %s\nBody: %s" uri meth "coucou" in
+    *)
+    Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:ics_string ()
+  | meth ->
+    Cohttp_lwt_unix.Server.respond_error ~status:`Method_not_allowed
+    ~body:(Printf.sprintf "Method %s is not suported" (Cohttp.Code.string_of_method meth)) ()
+  in
+  Cohttp_lwt_unix.Server.create ~mode:(`TCP (`Port 8080)) (Cohttp_lwt_unix.Server.make ~callback ())
+
+let () = ignore (Lwt_main.run server)
